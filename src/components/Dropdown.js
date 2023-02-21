4a097de0119ac58011f4b49b61da34896c058440
JasonPC <jasonwong287962@gmail.com>
2023-02-21 01:25:28 -0600

    need to implement a check if inside box

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,29 @@
-import React, {useEffect} from "react";
+import React, {useEffect,useState} from "react";
+import {
+  getFirestore,
+  collection,
+  addDoc,getDoc,
+  query,
+  orderBy,startAt,
+  limit,
+  onSnapshot,
+  setDoc,
+  updateDoc,
+  doc,
+  serverTimestamp,
+} from 'firebase/firestore';
+
+import { initializeFireStoreApp,getFireStoreData } from "./fireStoreMethod";
 
 const Dropdown = (props) => {
+  console.log(props, 'empty');
 
-  console.log(props);
+  const [imageID,setImageID] = useState(props.imageID);
+  const [clickedWaldo,setClickedWaldo] = useState(false);
+  const [imageData,setImageData] = useState(undefined);
+
+  initializeFireStoreApp();
+
   const {posX,posY} = props;
   useEffect(()=> {
     console.log('dropdown');
@@ -11,13 +32,59 @@ const Dropdown = (props) => {
 
    dropdown.style.left = `${posX}px`;
    dropdown.style.top = `${posY}px`;
+   if(imageID && imageID !== props.imageID){
+    console.log('update Image Data')
+    setImageData(getFireStoreData(imageID));
+
+   }
+
   },)
 
+  useEffect(()=> {
+  
+   if(imageID){
+    getFireStoreData(imageID).then((response) => response)
+    .then((data) => setImageData(data));
+   }
+  },[])
+
+  const clickedButton = (e) => {
+    let isInBox = false;;
+console.log(e.target.textContent);
+console.log(imageData);
+const positions = imageData.boxPosition.mapValue.fields
+console.log(positions);
+for (const key1 in positions) {
+      console.log(positions[key1].arrayValue.values);
+    const dataPoints = positions[key1].arrayValue.values;
+    dataPoints.forEach(element => {
+      switch(key1) {
+        case 'bottomL': 
+        
+            break;
+
+
+        case 'bottomR':
+
+      }
+      console.log(element.integerValue);
+    });
+
+ 
+}
+// const isMatch = positions.map(ele => console.log(ele)) 
+// Check person button click is same and coincides with person position
+
+if(imageData.personName.stringValue === e.target.textContent) {
+
+}
+
+  }
 
   return (
     <div className="col dropdown hidden" >
-        <button>Waldo</button>
-        <button>Guy</button>
+        <button onClick={clickedButton}>Waldo</button>
+        <button onClick={clickedButton}>Odlaw</button>
     </div>
   )
 }
