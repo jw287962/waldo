d5e1f695b6d64e70de620339fb720e4bf8fecb2f
JasonPC <jasonwong287962@gmail.com>
2023-02-20 23:43:05 -0600

    made firestore methods

---
+++ src/components/fireStoreMethod.js
@@ -0,0 +1,60 @@
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  addDoc,getDoc,
+  query, 
+  orderBy,startAt,
+  limit,
+  onSnapshot,
+  setDoc,
+  updateDoc,
+  doc,
+  serverTimestamp,
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwnotClaPasS5jB3Qb9zoLXT5Tbjag9o4",
+  authDomain: "waldo-d61a6.firebaseapp.com",
+  projectId: "waldo-d61a6",
+  storageBucket: "waldo-d61a6.appspot.com",
+  messagingSenderId: "223196428111",
+  appId: "1:223196428111:web:d1a41335026c2d7e4ab73a"
+};
+// Initialize Firebase
+initializeFireStoreApp();
+
+function initializeFireStoreApp() {
+  const app = initializeApp(firebaseConfig);
+  return app;
+}
+
+async function addCharacterPosition(boxPositionArray,imageName,collectionName,personName) {
+  // Add a new message entry to the Firebase database.
+  try {
+    await setDoc(doc(getFirestore(), `${collectionName}`,imageName), {
+      personName: personName,
+      boxPosition: boxPositionArray,
+      timestamp: serverTimestamp()
+    });
+  }
+  
+  catch(error) {
+    console.error('Error writing new message to Firebase Database', error);
+  }
+}
+
+
+async function getFireStoreData(){
+  const db = getFirestore();
+  const imagesRef = collection(db , "images");
+  const docSnap = await getDoc(doc(imagesRef, "where's waldo"));
+  
+  
+  const q = query(imagesRef, orderBy(""), startAt(docSnap));
+  console.log(q);
+}
+
+
+
+export  {addCharacterPosition, initializeFireStoreApp,getFireStoreData};
